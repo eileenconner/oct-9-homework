6e92684da42f06316be393c0789b5df37c8b9ab2
Eileen Conner <eileenconner@gmail.com>
2015-10-10 14:54:23 -0700

    HTML, CSS, and Flask app working. No further study yet.

--- skills-flask/application.py
+++ skills-flask/application.py
@@ -1,18 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# we need two routes here
+# one to serve the form and collect form data
+# the other to render/respond to the data submitted in the form
 
 
 @app.route("/")
 def index_page():
-    """Show an index page."""
+    """Ubermelon's job application system index page."""
+    return render_template("index.html")
 
-    return "<html><body>This is the homepage.</body></html>"
+
+@app.route("/application-form")
+def job_app_form():
+    """Render a job application form page."""
+
+    return render_template("application-form.html")
 
     # Alternately, we could make this a Jinja template in `templates/`
     # and return that result of rendering this, like:
     #
     # return render_template("index.html")
+
+
+@app.route("/application", methods=["POST"])
+def response_to_form():
+    """Render response to job application form submission."""
+
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    title = request.form.get("jobtitle")
+    salary = request.form.get("salaryreq")
+
+    # wrap the printed stuff in html to display
+    return render_template("application-response.html",
+                           first_name=first_name,
+                           last_name=last_name,
+                           title=title,
+                           salary=salary)
 
 
 if __name__ == "__main__":
