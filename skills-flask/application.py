a4ff7c855a59ab585114543f28e6484107871f7e
Eileen Conner <eileenconner@gmail.com>
2015-10-10 15:28:51 -0700

    Added form labels and melon-appropriate text colors.

--- skills-flask/application.py
+++ skills-flask/application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# we need two routes here
+# we need two routes here (besides basic index page)
 # one to serve the form and collect form data
 # the other to render/respond to the data submitted in the form
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 def index_page():
     """Ubermelon's job application system index page."""
+
     return render_template("index.html")
 
 
@@ -29,12 +30,13 @@ def job_app_form():
 def response_to_form():
     """Render response to job application form submission."""
 
+    # assign variables for form data
     first_name = request.form.get("firstname")
     last_name = request.form.get("lastname")
     title = request.form.get("jobtitle")
     salary = request.form.get("salaryreq")
 
-    # wrap the printed stuff in html to display
+    # return response form with appropriate values plugged in
     return render_template("application-response.html",
                            first_name=first_name,
                            last_name=last_name,
